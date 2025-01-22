631e885b0d4134eff9c7803aaf498b8c20773e73
rauffaizov <rauf.faizov.200271@gmail.com>
2025-01-22 10:56:15 +0100

    delelete no necessary stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+//index.js
+import {useState, useEffect} from "react"
+import {createRoot} from "react-dom/client"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Paypal() {
+  const [transfers, setTransfers] = useState(() => {
+    return JSON.parse(localStorage.getItem("transfers"))
+  })
+
+  useEffect(() => {
+    localStorage.setItem("transfers", JSON.stringify(transfers))
+  }, [transfers])
+
+  function handleReceiveClick() {
+    setTransfers([...transfers, 10])
+  }
+  
+  function handleSendClick() {
+    setTransfers([...transfers, -10])
+  }
+
+  function handleResetClick() {
+    setTransfers([])
+  }
+
+  const balance = transfers.reduce((sum, amount) => sum + amount, 0)
+
+  return <>
+    <button onClick={handleReceiveClick}>Принять 10</button>
+    <button onClick={handleSendClick}>Перевести 10</button>
+    <h2>Баланс: {balance}</h2>
+    <button onClick={handleResetClick}>Сброс</button>
+    <ul>
+      {transfers.map(
+        (transfer, index) => <li key={index}>{transfer}</li>
+      )}
+    </ul>
+  </>
 }
-
-export default App;
