851b043a079f862fab2dbcaf9ca327ea213940ee
rauffaizov <rauf.faizov.200271@gmail.com>
2025-01-30 06:45:28 +0100

    Add MySelect custom ui

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React, { useState } from "react";
 import "./style/style.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 export default function App() {
 
-  const [posts, setPosts] = useState([]); // Array to hold posts
+  const [posts, setPosts] = useState([]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -19,7 +20,16 @@ export default function App() {
   return (
     <div className="App">
       <PostForm create ={createPost}/>
-      <PostList remove={removePost} posts={posts} title={"React APP 3 hours!"}/>
+      <hr style={{margin:'15px 0 '}}/>
+      <div>
+        <MySelect
+        defaultValue="Sort by"
+        options ={[
+          {value: 'title', name: 'By name'},
+          {value: 'body', name: 'By description'}
+        ]}/>
+      </div>
+      {posts.length ? <PostList remove={removePost} posts={posts} title={"React APP 3 hours!"}/> : <h1 style={{textAlign:"center"}}>No posts</h1>}
     </div>
   )
 }
---
+++ src/components/UI/select/MySelect.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MySelect = ({options, defaultValue}) => {
+    return (
+        <select>
+            <option value="">{defaultValue}</option>
+            {options.map(option => 
+                <option key={option.value} value={option.value}>{option.name}</option>
+            )}
+        </select>
+    );
+};
+
+export default MySelect;
