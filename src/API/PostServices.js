ba5157858237cbe983c1689ee7e364a092f505f6
rauffaizov <rauf.faizov.200271@gmail.com>
2025-02-01 16:33:36 +0100

    made custom fetch hook

--- src/API/PostServices.js
+++ src/API/PostServices.js
@@ -2,14 +2,7 @@ import axios from "axios";
 
 export default class PostServices {
     static async getAll() {
-        try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-            return response.data;
-        } catch (error) {
-            console.error('Error:', error);
-            return [];
-        } finally {
-            console.log('finally');
-        }
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        return response.data;
     }
 }
--- src/App.js
+++ src/App.js
@@ -9,39 +9,29 @@ import MyModal from "./components/UI/Modal/MyModal";
 import { usePosts } from "./hooks/usePosts";
 import PostServices from "./API/PostServices";
 import Loader from "./components/UI/Loader/Loader";
+import useFetching from "./hooks/useFetching";
 
 export default function App() {
   // State variables
   const [posts, setPosts] = useState([]);
   const [filter,setFilter] = useState({sort:'', query:''});
-
   const [modal, setModal] = useState(false);
-
   const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
-  
-  const [isPostsLoading, setIsPostsLoading] = useState(false);
-  
+  const [fetchPost,isPostsLoading,postError] = useFetching(async () => {
+    const posts = await PostServices.getAll();
+    setPosts(posts);
+  })
+
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
 
   // Function to create a new post we send to PostForm component
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
-
-
-
-  useEffect(() => {
-    setIsPostsLoading(true);
-    (async () => {
-      setTimeout(async() => {
-        const posts = await PostServices.getAll();
-        setPosts(posts);
-        setIsPostsLoading(false);
-      }, 1000);
-    })();
-  },[])
-
-
 
   // Function to remove a post
   const removePost = (post) => {
@@ -59,6 +49,7 @@ export default function App() {
       </MyModal>
       <hr style={{ margin: '15px 0' }} />
       <PostFilter filter={filter} setFilter={setFilter} />
+      {postError && <h1>Error: {postError}</h1>}
       {isPostsLoading?<div style={{display: 'flex', justifyContent: 'center', marginTop: "50px"}}><Loader/></div>:<PostList remove={removePost} posts={sortedAndSearchPosts} title={"React APP 3 hours!"} />}
     </div>
   );
